107f8ae61a69daf1d8c8d85953278b4868c93824
Alex <therebeloper@gmail.com>
2018-12-20 17:35:11 +0200

    Initial Commit

---
+++ CollectionKitPro/NewsfeedController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsfeedController.swift
+//  CollectionKitPro
+//
+//  Created by Alex Nagy on 20/12/2018.
+//  Copyright © 2018 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class NewsfeedController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
